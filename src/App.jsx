69117b70b34b9e726ad334787814704f055b63d8
srikishore5727 <srikishore9080676683@gmail.com>
2023-12-28 14:06:18 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
+import React from 'react';
 import './App.css';
-import elephant from "./images/elephant.jpeg";
+import elephant from './images/elephant.jpeg';
 
-const imageData = ()=>{
+const imageData = () => {
   let data = [
-    {
-      id:1,
-      img:elephant
-    },
-    {
-      id:2,
-      img:elephant
-    },
-    {
-      id:3,
-      img:elephant
-    },
-    {
-      id:4,
-      img:elephant
-    }
-  ]
+    { id: 1, img: elephant },
+    { id: 2, img: elephant },
+    { id: 3, img: elephant },
+    { id: 4, img: elephant },
+  ];
   return data;
-}
+};
 
 function App() {
-  // code here
+  return (
+    <>
+      <h1 style={{textAlign:"center"}}>Kalvium Gallery</h1>
+      <div className="image-container">
+        {imageData().map((item) => (
+          <img key={item.id} src={item.img} alt={`elephant-${item.id}`} />
+        ))}
+      </div>
+    </>
+
+  );
 }
 
 export default App;
--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -1,30 +1,25 @@
-import { Component } from "react";
-import "./App.css"
+import React, { Component } from "react";
+import "./App.css";
 import elephant from "./images/elephant.jpeg";
 
-export default class AppClass extends Component{
-  
-  imageData = ()=>{
+export default class AppClass extends Component {
+  imageData = () => {
     let data = [
-      {
-        id:1,
-        img:elephant
-      },
-      {
-        id:2,
-        img:elephant
-      },
-      {
-        id:3,
-        img:elephant
-      },
-      {
-        id:4,
-        img:elephant
-      }
-    ]
+      { id: 1, img: elephant },
+      { id: 2, img: elephant },
+      { id: 3, img: elephant },
+      { id: 4, img: elephant },
+    ];
     return data;
-  }
+  };
 
-  // code here
+  render() {
+    return (
+      <div className="image-container">
+        {this.imageData().map((item) => (
+          <img key={item.id} src={item.img} alt={`elephant-${item.id}`} />
+        ))}
+      </div>
+    );
+  }
 }
